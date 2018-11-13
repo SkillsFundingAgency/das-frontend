797f3fa112240dc70687ef312e1f84d5e7b4ac19
James King <hello@jamesmichaelking.com>
2018-11-13 13:49:26 +0000

    SASS structure for campaigns

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ require('./tasks/gulp/default')
 require('./tasks/gulp/legacy')
 require('./tasks/gulp/campaign')
 
-gulp.task('legacy', ['copy-legacy-template-assets', 'js-legacy', 'sass-legacy']);
-gulp.task('default', ['copy-assets', 'copy-js', 'sass', 'watch']);
-gulp.task('campaign', ['watch-campaign']);
+gulp.task('legacy',   ['copy-legacy-template-assets', 'js-legacy', 'sass-legacy']);
+gulp.task('default',  ['copy-assets', 'copy-js', 'sass', 'watch']);
+gulp.task('campaign', ['sass-campaign', 'watch-campaign']);
--- tasks/gulp/campaign.js
+++ tasks/gulp/campaign.js
@@ -1,6 +1,18 @@
 'use strict'
 const gulp = require('gulp')
+const sass = require('gulp-sass');
+const paths = require('../../config/paths.json')
+const sassOptions = require('../../config/sassOptions.js')
 
 gulp.task('watch-campaign', () => {
-
+  gulp.watch(paths.src.campaign, ['sass-campaign'])
+    .on('change', (event) => {
+      console.log(`File ${event.path} was ${event.type}, running tasks...`);
+    });
 })
+
+gulp.task('sass-campaign', () => gulp
+  .src(paths.src.campaign)
+  .pipe(sass(sassOptions))
+  .pipe(gulp.dest(paths.dist.campaign)));
+
