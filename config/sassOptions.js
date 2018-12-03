0865feee0a7ea8bf9bb3f13e110102f811b4625f
James King <hello@jamesmichaelking.com>
2018-12-03 22:18:04 +0000

    Launcher component homepage

--- config/sassOptions.js
+++ config/sassOptions.js
@@ -2,7 +2,6 @@ let sassOptions;
 
 sassOptions = {
   errLogToConsole: true,
-  outputStyle: 'compressed',
   includePaths: [
     'node_modules/govuk_template_mustache/source/assets/stylesheets',
     'node_modules/govuk_frontend_toolkit/stylesheets',
