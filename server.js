c42b4fce00286727fde9bcfac67d0275e2edee1a
James King <hello@jamesmichaelking.com>
2019-02-08 10:47:51 +0000

    Changes to local text server to allow site to be used as a CDN

--- server.js
+++ server.js
@@ -22,8 +22,26 @@ nunjucks.configure(
   noCache: true,
 });
 
+
 app.set('view engine', 'html');
-app.use('/dist', express.static('dist'));
+
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:4737');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(express.static('dist'));
 
 routes.bind(app);
 
