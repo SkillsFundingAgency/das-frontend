a4fc5b9ba06cb584d1b1ecc53cca0df322fa33c3
James King <hello@jamesmichaelking.com>
2019-03-28 10:56:14 +0000

    Removed legacy paths

--- config/sassOptions.js
+++ config/sassOptions.js
@@ -4,9 +4,6 @@ sassOptions = {
   errLogToConsole: true,
   outputStyle: 'compressed',
   includePaths: [
-    'node_modules/govuk_template_mustache/source/assets/stylesheets',
-    'node_modules/govuk_frontend_toolkit/stylesheets',
-    'node_modules/govuk-elements-sass/public/sass',
     'node_modules/govuk-frontend',
     'node_modules/accessible-autocomplete/src',
     'src/sass'
--- config/sassOptionsCampaign.js
+++ config/sassOptionsCampaign.js
@@ -4,9 +4,6 @@ sassOptionsCampaign = {
   errLogToConsole: true,
   outputStyle: 'compressed',
   includePaths: [
-    'node_modules/govuk_template_mustache/source/assets/stylesheets',
-    'node_modules/govuk_frontend_toolkit/stylesheets',
-    'node_modules/govuk-elements-sass/public/sass',
     'node_modules/govuk-frontend',
     'node_modules/plyr/src/sass',
     'src/sass'
