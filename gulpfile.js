c192acc44b81e44ababfbc7ab93b4bbf46efbb2e
James King <hello@jamesmichaelking.com>
2019-03-28 10:52:10 +0000

    Removed legacy GDS files from the front-end

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 const gulp = require('gulp');
 
 require('./tasks/gulp/default')
-require('./tasks/gulp/legacy')
 require('./tasks/gulp/campaign')
 require('./tasks/gulp/dasJs')
 
-gulp.task('legacy',   ['copy-legacy-template-assets', 'js-legacy', 'sass-legacy']);
 gulp.task('default',  ['copy-assets', 'copy-js', 'sass', 'watch', 'js:compile', 'watch-js-das-all', 'copy-libs']);
 gulp.task('campaign', ['sass-campaign', 'watch-campaign', 'js-campaign','image-campaign', 'copy-plyr-js']);
