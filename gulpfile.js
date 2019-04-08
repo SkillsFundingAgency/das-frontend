b50759f8b5fd66f83fcc46160ccae0d33c28b590
James King <hello@jamesmichaelking.com>
2019-04-08 12:05:47 +0100

    Gulp task refactor for DAS

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ require('./tasks/gulp/default')
 require('./tasks/gulp/campaign')
 require('./tasks/gulp/dasJs')
 
-const defaultBuildTasks = ['copy-assets', 'copy-js', 'sass', 'js:compile', 'copy-libs', 'copy-rename-autocomplete-css'];
-const defaultWatchTasks = ['watch', 'watch-js-das-all'];
+const defaultBuildTasks = ['das-copy-libs', 'das-compile-sass', 'das-compile-js', 'das-compile-js-components'];
+const defaultWatchTasks = ['das-watch-sass', 'das-watch-js'];
 
 const campaignBuildTasks = ['sass-campaign', 'js-campaign','image-campaign', 'copy-plyr-js', 'copy-campaign-libs'];
 const campaignWatchTasks = ['watch-campaign'];
