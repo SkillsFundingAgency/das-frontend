3df72aa6feca1ed618c17cde0d12720c641a664f
James King <hello@jamesmichaelking.com>
2019-10-03 00:13:54 +0100

    Restore plugins

--- tasks/gulp/dasJs.js
+++ tasks/gulp/dasJs.js
@@ -50,6 +50,7 @@ var minifyJs = function (isDist) {
   return gulp.src(srcFile)
     .pipe(rollup({
       name: 'DASFrontend',
+      plugins: [resolve(), commonjs()],
       legacy: true,
       format: 'umd',
     })).on('error', function (e) { console.log(e) })
