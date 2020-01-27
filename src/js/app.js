bd9516ef2fd557d427bacbf63a735d7b866e1795
James King <hello@jamesmichaelking.com>
2020-01-27 11:02:41 +0000

    WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,25 @@
 var dasJs = dasJs || {};
 
+dasJs = {
+  backLink: function () {
+    let backLinkControl = $('<a>')
+      .attr({'href': '#', 'class': 'govuk-back-link'})
+      .text('Back')
+      .on('click', function (e) {
+        window.history.back();
+        e.preventDefault();
+      });
+    $('.das-js-back-link').replaceWith(backLinkControl);
+  }
+}
+
+
 if ($('#das-user-navigation')) {
-  dasJs.userNavigation.init()
+  dasJs.userNavigation.init();
+}
+
+if ($('.das-js-back-link')) {
+  dasJs.backLink();
 }
 
 dasJs.forms.init();
