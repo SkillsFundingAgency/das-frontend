1f09a25d63cfe4d10af574f83941737cd43372ad
James King <hello@jamesmichaelking.com>
2020-02-13 11:14:00 +0000

    Updates to the modal version of the cookie settings

--- src/das-all.js
+++ src/das-all.js
@@ -51,7 +51,8 @@ function initAll() {
   // Cookie Settings Page
   var $cookieSettings = document.querySelector('[data-module="cookie-settings"]');
   if ($cookieSettings != null) {
-    new CookieSettings($cookieSettings);
+    var $cookieSettingsOptions = $cookieSettings.dataset.options;
+    new CookieSettings($cookieSettings, $cookieSettingsOptions);
   }
 
   var $accordions = document.querySelectorAll('[data-module="accordion"]');
