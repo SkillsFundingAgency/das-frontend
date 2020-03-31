3ec7ee2e93dc4d939fb64de4281ec4872a69114c
James King <hello@jamesmichaelking.com>
2020-03-31 14:20:29 +0100

    Restructure front-end files - separate DAS from Campaign

--- tasks/gulp/das.js
+++ tasks/gulp/das.js
@@ -7,35 +7,34 @@ const paths = require('../../config/paths.json');
 const sassOptions = require('../../config/sassOptions.js');
 
 gulp.task('das-watch-sass', function() {
-  return gulp.watch(paths.src.default, gulp.series('das-compile-sass')).on('change', function (file) {
+  return gulp.watch(paths.src.dasSass, gulp.series('das-compile-sass')).on('change', function (file) {
     console.log(`File ${file} was changed, running tasks...`);
   });
 });
 
 gulp.task('das-compile-sass', function() {
-  return gulp.src(paths.src.default)
+  return gulp.src(paths.src.dasSass)
     .pipe(sass(sassOptions))
-    .pipe(gulp.dest(paths.dist.default));
+    .pipe(gulp.dest(paths.dist.dasCss));
 });
 
 gulp.task('das-copy-images', function() {
-  return gulp.src(paths.src.defaultImages).pipe(gulp.dest(paths.dist.defaultImages));
+  return gulp.src(paths.src.dasImages).pipe(gulp.dest(paths.dist.dasImages));
 });
 
 gulp.task('das-copy-libs', (done) => {
-  gulp.src(['./src/libs/*.js']).pipe(gulp.dest('./dist/libs/'));
   gulp.src(['./node_modules/govuk-frontend/govuk/assets/**/*','./src/assets/**/*']).pipe(gulp.dest('./dist/assets/'));
   gulp.src(['./node_modules/govuk-frontend/govuk/all.js']).pipe(gulp.dest('./dist/libs/govuk-frontend'));
   gulp.src(['./node_modules/accessible-autocomplete/dist/accessible-autocomplete.min.js']).pipe(gulp.dest('./dist/libs/accessible-autocomplete'));
-  gulp.src(['./src/libs/das-postcode/lookupService.js']).pipe(gulp.dest('./dist/libs/das-postcode'));
+  gulp.src([paths.src.dasJsLibPath + '/das-postcode/lookupService.js']).pipe(gulp.dest('./dist/libs/das-postcode'));
   gulp.src(['./node_modules/html5shiv/dist/html5shiv.min.js']).pipe(gulp.dest('./dist/libs/html5shiv'));
   gulp.src(['./node_modules/jquery/dist/jquery.min.js']).pipe(gulp.dest('./dist/libs/jquery'));
   gulp.src(['./node_modules/jquery-ui-dist/jquery-ui.min.js']).pipe(gulp.dest('./dist/libs/jquery-ui'));
   gulp.src(['./node_modules/jquery-validation/dist/jquery.validate.min.js']).pipe(gulp.dest('./dist/libs/jquery-validation'));
   gulp.src(['./node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js']).pipe(gulp.dest('./dist/libs/jquery-validation-unobtrusive'));
   gulp.src(['./node_modules/select2/dist/js/select2.min.js']).pipe(gulp.dest('./dist/libs/select2'));
-  gulp.src(['./src/libs/select2/style.css']).pipe(gulp.dest('./dist/libs/select2'));
+  gulp.src([paths.src.dasJsLibPath + '/select2/style.css']).pipe(gulp.dest('./dist/libs/select2'));
   gulp.src(['./node_modules/jquery.are-you-sure/jquery.are-you-sure.js']).pipe(rename('jquery-are-you-sure.js')).pipe(gulp.dest('./dist/libs/jquery-are-you-sure')); // Rename to remove dots from filename, avoid confusion with the CDN script
-  gulp.src(['./node_modules/accessible-autocomplete/src/autocomplete.css']).pipe(rename('_autocomplete.scss')).pipe(gulp.dest('./src/sass/libs/')); // Rename CSS file to SCSS so can be imported into application SCSS
+  gulp.src(['./node_modules/accessible-autocomplete/src/autocomplete.css']).pipe(rename('_autocomplete.scss')).pipe(gulp.dest('./src/das/sass/libs/')); // Rename CSS file to SCSS so can be imported into application SCSS
   done();
 });
