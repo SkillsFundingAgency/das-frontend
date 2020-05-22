b92090ea4d8dcd0185e3668a6e5e2b7d9aae9518
Darren Harridence <darren.harridence@btinternet.com>
2020-05-22 14:47:15 +0100

    Class name and url link updates

--- src/das/javascript/components/jumpToScroll/_jumpToScroll.js
+++ src/das/javascript/components/jumpToScroll/_jumpToScroll.js
@@ -1,5 +1,7 @@
 function JumpToScroll(ele) {
-    ele.insertAdjacentHTML('afterend', '<a class="govuk-link govuk-link--no-visited-state app-back-to-top__link" href="#main-content"><svg role="presentation" focusable="false" class="app-back-to-top__icon" xmlns="http://www.w3.org/2000/svg" width="22" height="17" viewBox="0 0 22 17"><path fill="currentColor" d="M6.5 0L0 6.5 1.4 8l4-4v12.7h2V4l4.3 4L13 6.4z"></path></svg><span>Back to top</span></a>');
+
+    ele.classList.add('das-jump-to-scroll');
+    ele.insertAdjacentHTML('afterend', '<a class="govuk-link govuk-link--no-visited-state" href="#main-content"><svg role="presentation" focusable="false" class="app-back-to-top__icon" xmlns="http://www.w3.org/2000/svg" width="22" height="17" viewBox="0 0 22 17"><path fill="currentColor" d="M6.5 0L0 6.5 1.4 8l4-4v12.7h2V4l4.3 4L13 6.4z"></path></svg><span>Back to top</span></a>');
 
     function bottomP(){
         return ele.getBoundingClientRect().bottom + document.documentElement.scrollTop;
@@ -19,9 +21,9 @@ function JumpToScroll(ele) {
 
     function scrollFnt(bp) {
         if (document.body.scrollTop > bp || document.documentElement.scrollTop > bp) {
-            ele.classList.add('das-show-jump-to-scroll');
+            ele.classList.add('das-jump-to-scroll--show');
         } else {
-            ele.classList.remove('das-show-jump-to-scroll');
+            ele.classList.remove('das-jump-to-scroll--show');
         }
     }
 
