085c938256f09c0bbd9aa4c6bda8ca4bfef04f05
James King <hello@jamesmichaelking.com>
2020-08-27 16:53:21 +0100

    Cookie Banner styles

---
+++ src/fiu/components/cookie-banner/cookie-banner.js
@@ -0,0 +1,66 @@
+function CookieBanner (module) {
+  this.module = module;
+  this.settings = {
+    seenCookieName: 'DASSeenCookieMessage',
+    env: window.GOVUK.getEnv(),
+    cookiePolicy: {
+      AnalyticsConsent: false,
+      MarketingConsent: false
+    }
+  }
+  if ( !window.GOVUK.cookie(this.settings.seenCookieName + this.settings.env) ) {
+    this.start()
+  }
+}
+
+CookieBanner.prototype.start = function () {
+  console.log('start')
+  this.module.cookieBanner = this.module
+  this.module.cookieBannerInnerWrap = this.module.querySelector('.fiu-cookie-banner__message')
+  this.module.cookieBannerConfirmationMessage = this.module.querySelector('.fiu-cookie-banner__confirmation')
+  this.setupCookieMessage()
+}
+
+CookieBanner.prototype.setupCookieMessage = function () {
+  this.module.hideLink = this.module.querySelector('a[data-hide-cookie-banner]')
+  this.module.acceptCookiesButton = this.module.querySelector('a[data-accept-cookies]')
+
+  if (this.module.hideLink) {
+    this.module.hideLink.addEventListener('click', this.hideCookieBanner.bind(this))
+  }
+
+  if (this.module.acceptCookiesButton) {
+    this.module.acceptCookiesButton.addEventListener('click', this.acceptAllCookies.bind(this))
+  }
+  this.showCookieBanner()
+}
+
+CookieBanner.prototype.showCookieBanner = function () {
+  const cookiePolicy = this.settings.cookiePolicy,
+    that = this;
+  this.module.cookieBanner.style.display = 'block';
+  console.log(123)
+
+  // Create the default cookies based on settings
+  Object.keys(cookiePolicy).forEach(function (cookieName) {
+    window.GOVUK.cookie(cookieName + that.settings.env, cookiePolicy[cookieName].toString(), { days: 365 })
+  });
+
+}
+
+CookieBanner.prototype.hideCookieBanner = function () {
+  this.module.cookieBanner.style.display = 'none';
+  window.GOVUK.cookie(this.settings.seenCookieName + this.settings.env, true, { days: 365 })
+}
+
+CookieBanner.prototype.acceptAllCookies = function () {
+  const that = this;
+  this.module.cookieBannerInnerWrap.style.display = 'none';
+  this.module.cookieBannerConfirmationMessage.style.display = 'block';
+
+  window.GOVUK.cookie(this.settings.seenCookieName + this.settings.env, true, { days: 365 })
+
+  Object.keys(this.settings.cookiePolicy).forEach(function (cookieName) {
+    window.GOVUK.cookie(cookieName + that.settings.env, true, { days: 365 })
+  });
+}
