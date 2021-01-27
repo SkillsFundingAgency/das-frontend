c2fb66ad0be190738d592c8b07d9f8556e96dfa4
James King <hello@jamesmichaelking.com>
2021-01-27 14:32:05 +0000

    Routes for local views

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,12 @@
 module.exports = {
   bind: function (app) {
+
     app.get('/', function (req, res) {
       res.render('index')
+    })
+
+    app.get('/local/:page', function (req, res) {
+      res.render('local/' + req.params.page)
     })
 
   }
