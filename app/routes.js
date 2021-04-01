fb9c5090d711579ddadabe9b32b651e40a86240e
James King <hello@jamesmichaelking.com>
2021-04-01 16:17:05 +0100

    Basic views and routes for local form dev

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,23 @@ module.exports = {
       res.render('index')
     })
 
+    app.get('/form', function (req, res) {
+      res.render('form')
+    })
+
+    app.post('/form', function (req, res) {
+      // Do something
+      res.redirect('/playback')
+    })
+
+    app.get('/playback', function (req, res) {
+      res.render('playback')
+    })
+
     app.get('/local/:page', function (req, res) {
       res.render('local/' + req.params.page)
     })
+
 
   }
 }
