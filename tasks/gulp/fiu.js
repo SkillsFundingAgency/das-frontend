0be10e0cc0a6f73e58ea5e93a146b3531b9eff59
James King <hello@jamesmichaelking.com>
2021-11-23 11:30:27 +0000

    Remove unused packages

--- tasks/gulp/fiu.js
+++ tasks/gulp/fiu.js
@@ -4,7 +4,6 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
 const sass = require('gulp-sass')(require('node-sass'))
-const rename = require('gulp-rename');
 const paths = require('../../config/paths.json')
 const sassOptionsFiu = require('../../config/sassOptionsFiu.js')
 
