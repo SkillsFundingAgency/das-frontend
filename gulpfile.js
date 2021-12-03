329384c78f7278ed2e655b8a1eff46d01b042ebd
James King <hello@jamesmichaelking.com>
2021-12-03 14:53:46 +0000

    Apprentice commitments frontend

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ const gulp = require('gulp');
 
 require('./tasks/gulp/das');
 require('./tasks/gulp/fiu');
+require('./tasks/gulp/apprentice');
 require('./tasks/gulp/dasJs');
 
 const defaultBuildTasks = gulp.series('das-copy-libs', 'das-copy-images', 'das-compile-sass', 'das-compile-js', 'das-compile-js-components', 'das-compile-js-components-dev');
 const defaultWatchTasks = gulp.parallel('das-watch-sass', 'das-watch-js');
 const fiuBuildTasks = gulp.series('fiu-compile-sass', 'fiu-compile-js', 'fiu-copy-images');
 const fiuWatchTasks = gulp.parallel('fiu-watch-sass', 'fiu-watch-js');
+const apprenticeBuildTasks = gulp.series('apprentice-compile-sass', 'apprentice-compile-js');
+const apprenticeWatchTasks = gulp.parallel('apprentice-watch-sass', 'apprentice-watch-js');
 
 gulp.task('das',  gulp.series(defaultBuildTasks, defaultWatchTasks));
 gulp.task('fiu', gulp.series(fiuBuildTasks, fiuWatchTasks));
-gulp.task('build', gulp.series(defaultBuildTasks, fiuBuildTasks));
+gulp.task('apprentice', gulp.series(apprenticeBuildTasks, apprenticeWatchTasks));
+gulp.task('build', gulp.series(defaultBuildTasks, fiuBuildTasks, apprenticeBuildTasks));
 
