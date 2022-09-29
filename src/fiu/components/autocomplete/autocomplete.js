5b4c1ffeabf4783b2e2e8c89d017104867af2649
James King <hello@jamesmichaelking.com>
2022-09-29 14:21:13 +0100

    Autocomplete JS update

--- src/fiu/components/autocomplete/autocomplete.js
+++ src/fiu/components/autocomplete/autocomplete.js
@@ -1,5 +1,6 @@
 function Autocomplete(select) {
     this.select = select;
+    this.selectId = this.select.id
 }
 
 Autocomplete.prototype.init = function () {
@@ -8,6 +9,20 @@ Autocomplete.prototype.init = function () {
         minLength: 2,
         defaultValue: '',
         displayMenu: 'overlay',
-        placeholder: ''
+        placeholder: '',
+        onConfirm: (opt) => {
+            const txtInput = document.querySelector('#' + this.selectId);
+            const searchString = opt || txtInput.value
+            const requestedOption = [].filter.call(this.select.options,
+                function (option) {
+                    return (option.textContent || option.innerText) === searchString
+                }
+            )[0];
+            if (requestedOption) {
+                requestedOption.selected = true;
+            } else {
+                this.select.selectedIndex = 0;
+            }
+        }
     });
 }
