bce1acef2f87ab2d90638dfe442a2ecb3226cc54
James King <hello@jamesmichaelking.com>
2024-01-09 09:21:20 +0000

    Fix breaking changes from govuk frontend v5

--- config/sassOptions.js
+++ config/sassOptions.js
@@ -6,7 +6,7 @@ sassOptions = {
   errLogToConsole: true,
   outputStyle: 'compressed',
   includePaths: [
-    'node_modules/govuk-frontend/govuk',
+    'node_modules/govuk-frontend/dist/govuk',
     'src/sass',
     'src/sass/libs'
   ]
