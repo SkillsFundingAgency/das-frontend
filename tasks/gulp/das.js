50efddeb5483961cd7ffa562c5453d2bbaf3d9f9
James King <hello@jamesmichaelking.com>
2024-02-15 20:20:58 +0000

    Use dart-sass instead of node-sass

--- tasks/gulp/das.js
+++ tasks/gulp/das.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('node-sass'))
+const sass = require('gulp-sass')(require('sass'))
 const rename = require('gulp-rename');
 const paths = require('../../config/paths.json');
 const sassOptions = require('../../config/sassOptions.js');
