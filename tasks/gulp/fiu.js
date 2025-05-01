c4670e3159489b342e4993c136b5c0086ebf750a
James King <hello@jamesmichaelking.com>
2025-05-01 14:50:14 +0100

    FIU JS file

--- tasks/gulp/fiu.js
+++ tasks/gulp/fiu.js
@@ -19,7 +19,7 @@ gulp.task('fiu-compile-sass', () => gulp
   .pipe(gulp.dest(paths.dist.fiuCss)));
 
 gulp.task('fiu-watch-js', function() {
-  gulp.watch([paths.src.fiuJsLibs, paths.src.fiuJs, paths.src.fiuJsInit], gulp.series('fiu-compile-js'))
+  gulp.watch([paths.src.fiuJsLibs, paths.src.fiuJsParent, paths.src.fiuJsInit], gulp.series('fiu-compile-js'))
     .on('change', function (path) {
       console.log(`File ${path} was changed, running tasks...`);
     });
